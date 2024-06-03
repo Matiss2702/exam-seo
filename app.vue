c6494c21cfed3341def299371a21968a51a6ffeb
Matiss2702 <matiss.haillouy@gmail.com>
2024-06-03 22:33:14 +0200

    Quick updates, homepage ended

--- app.vue
+++ app.vue
@@ -1,26 +1,28 @@
 <template>
-    <div class="font-neco flex flex-col gap-y-8">
-        <Navbar />
-        <NuxtPage />
-        <Footer />
-    </div>
+  <div class="font-neco">
+    <Navbar />
+    <main class="mt-4 mb-12">
+      <NuxtPage />
+    </main>
+    <Footer />
+  </div>
 </template>
 
 <style scoped>
 #app {
-    display: flex;
-    flex-direction: column;
-    min-height: 100vh;
-    /* La hauteur minimum de la page est la hauteur de la fenêtre du navigateur */
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  /* La hauteur minimum de la page est la hauteur de la fenêtre du navigateur */
 }
 
 main {
-    flex-grow: 1;
-    /* Le contenu principal prendra toute la hauteur restante */
+  flex-grow: 1;
+  /* Le contenu principal prendra toute la hauteur restante */
 }
 
 .footer {
-    margin-top: auto;
-    /* Le footer sera toujours en bas de la page */
+  margin-top: auto;
+  /* Le footer sera toujours en bas de la page */
 }
 </style>
--- components/HomeSourceCard.vue
+++ components/HomeSourceCard.vue
@@ -1,14 +1,23 @@
 <template>
-    <div class="border border-primary-200 relative rounded-xl">
-        <NuxtImg class="size-full object-cover rounded-xl" :src="imageSrc" />
-        <nuxt-link :to="path" class="text-neutral-50 absolute left-0 bottom-0">{{ title }}</nuxt-link>
-    </div>
+  <a
+    :href="path"
+    class="relative overflow-hidden rounded-xl text-neutral-950 group/homesourcecard"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    <NuxtImg class="object-cover size-full rounded-xl" :src="imageSrc" />
+    <span
+      class="absolute bottom-0 z-10 w-full py-2 text-xl text-center transition-all duration-300 translate-y-12 bg-primary-950/85 font-pally text-neutral-50 group-hover/homesourcecard:-translate-y-0"
+    >
+      {{ title }}</span
+    >
+  </a>
 </template>
 
 <script setup>
 defineProps({
-    title: String,
-    path: String,
-    imageSrc: String,
+  title: String,
+  path: String,
+  imageSrc: String,
 });
 </script>

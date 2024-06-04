a589a533e1808ccef41a583d921bd0d2c8ccbbcf
Matiss2702 <matiss.haillouy@gmail.com>
2024-06-04 18:44:45 +0200

    Pages WaterGestion and Biodiversity ended

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,60 +1,57 @@
 <template>
-    <!-- Div qui doit faire la taille des différentes navbar !!! -->
-    <div class="h-[70.8px] md:h-[86.39px]" />
-    <!-- Menu de navigation -->
-    <header class="py-3 text-sm fixed top-0 left-0 right-0 z-50 backdrop-blur-lg bg-white/65 max-md:px-0">
-        <!-- Navbar Tablette / Pc  -->
-        <nav class="container">
-            <ul class="flex gap-x-8 items-center font-pally text-base max-md:hidden">
-                <li class="hover:text-primary-400">
-                    <nuxt-link to="/conservationdessols" class="">Conservation des Sols</nuxt-link>
-                </li>
-                <li class="hover:text-primary-400">
-                    <nuxt-link to="/gestiondeleau" class="">Gestion de l'Eau</nuxt-link>
-                </li>
-                <li class="flex-1 justify-center flex">
-                    <nuxt-link class="hover:scale-105 duration-300 transition-transform" to="/">
-                        <NuxtImg width="48" src="/logo.svg" />
-                    </nuxt-link>
-                </li>
-                <li class="hover:text-primary-400">
-                    <nuxt-link to="/biodiversite" class="">Biodiversité</nuxt-link>
-                </li>
-                <li class="hover:text-primary-400">
-                    <nuxt-link to="#" class="">Terrain Confus</nuxt-link>
-                </li>
-            </ul>
-        </nav>
-        <!-- Navbar Mobile -->
-        <div class="flex justify-between md:hidden items-center container">
-            <nuxt-link class="hover:scale-105 duration-300 transition-transform" to="/">
-                <NuxtImg width="36" src="/logo.svg" />
-            </nuxt-link>
-            <button
-                @click="toggleNav"
-                class="flex justify-center hover:text-primary-400 transition-colors duration-200"
-            >
-                <LucideMenu size="24" />
-            </button>
-        </div>
-        <!-- Menu Navbar Mobile -->
-        <nav :class="!isNavOpen && 'hidden'">
-            <ul class="flex flex-col gap-y-8 items-center font-pally text-base">
-                <li class="hover:text-primary-500">
-                    <nuxt-link to="/conservationdessols" class="">Conservation des Sols</nuxt-link>
-                </li>
-                <li class="hover:text-primary-500">
-                    <nuxt-link to="/gestiondeleau" class="">Gestion de l'Eau</nuxt-link>
-                </li>
-                <li class="hover:text-primary-500">
-                    <nuxt-link to="/biodiversite" class="">Biodiversité</nuxt-link>
-                </li>
-                <li class="hover:text-primary-500">
-                    <nuxt-link to="#" class="">Terrain Confus</nuxt-link>
-                </li>
-            </ul>
-        </nav>
-    </header>
+  <!-- Div qui doit faire la taille des différentes navbar !!! -->
+  <div class="h-[70.8px] md:h-[86.39px]" />
+  <!-- Menu de navigation -->
+  <header class="fixed top-0 left-0 right-0 z-50 py-3 text-sm backdrop-blur-lg bg-white/65 max-md:px-0">
+    <!-- Navbar Tablette / Pc  -->
+    <nav class="container">
+      <ul class="flex items-center text-base gap-x-8 font-pally max-md:hidden">
+        <li class="hover:text-primary-400">
+          <nuxt-link to="/conservationdessols" class="">Conservation des Sols</nuxt-link>
+        </li>
+        <li class="hover:text-primary-400">
+          <nuxt-link to="/gestiondeleau" class="">Gestion de l'Eau</nuxt-link>
+        </li>
+        <li class="flex justify-center flex-1">
+          <nuxt-link class="transition-transform duration-300 hover:scale-105" to="/">
+            <NuxtImg width="48" src="/logo.svg" />
+          </nuxt-link>
+        </li>
+        <li class="hover:text-primary-400">
+          <nuxt-link to="/biodiversite" class="">Biodiversité</nuxt-link>
+        </li>
+        <li class="hover:text-primary-400">
+          <nuxt-link to="#" class="">Terrain Confus</nuxt-link>
+        </li>
+      </ul>
+    </nav>
+    <!-- Navbar Mobile -->
+    <div class="container flex items-center justify-between md:hidden">
+      <nuxt-link class="transition-transform duration-300 hover:scale-105" to="/">
+        <NuxtImg width="36" src="/logo.svg" />
+      </nuxt-link>
+      <button @click="toggleNav" class="flex justify-center transition-colors duration-200 hover:text-primary-400">
+        <LucideMenu size="24" />
+      </button>
+    </div>
+    <!-- Menu Navbar Mobile -->
+    <nav :class="!isNavOpen && 'hidden'">
+      <ul class="flex flex-col items-center text-base gap-y-8 font-pally z-[9999]">
+        <li class="hover:text-primary-500">
+          <nuxt-link to="/conservationdessols" class="">Conservation des Sols</nuxt-link>
+        </li>
+        <li class="hover:text-primary-500">
+          <nuxt-link to="/gestiondeleau" class="">Gestion de l'Eau</nuxt-link>
+        </li>
+        <li class="hover:text-primary-500">
+          <nuxt-link to="/biodiversite" class="">Biodiversité</nuxt-link>
+        </li>
+        <li class="hover:text-primary-500">
+          <nuxt-link to="#" class="">Terrain Confus</nuxt-link>
+        </li>
+      </ul>
+    </nav>
+  </header>
 </template>
 
 <script setup>
@@ -63,6 +60,6 @@ import { ref } from "vue";
 const isNavOpen = ref(false);
 
 const toggleNav = () => {
-    isNavOpen.value = !isNavOpen.value;
+  isNavOpen.value = !isNavOpen.value;
 };
 </script>

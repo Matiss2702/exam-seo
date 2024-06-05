d8595c81495269a1fff089fd089b8c3868d56656
nikolasbaele <nikolasbaele@gmail.com>
2024-06-05 10:24:29 +0200

    Footer Pages UI ended

--- components/FooterPages.vue
+++ components/FooterPages.vue
@@ -1,21 +1,27 @@
 <template>
-  <div class="grid items-center grid-cols-1 gap-4 sm:grid-cols-2 sm:gap-6 group/conservationhero">
-    <div class="flex flex-col gap-y-1 sm:gap-y-2.5 lg:gap-y-3.5">
-      <div>
-        <h1 class="">{{ title }}</h1>
-        <NuxtImg class="object-cover w-full transition-transform duration-300 h-72 rounded-3xl" :src="firstImageSrc" />
-      </div>
-      <div>
-        <p class="">
+  <div class="flex flex-col gap-y-8">
+    <div class="flex flex-col gap-y-4">
+      <h1 class="title text-center">{{ title }}</h1>
+      <NuxtImg class="object-cover w-full transition-transform duration-300 h-72 rounded-3xl" :src="firstImageSrc" />
+    </div>
+    <div class="grid grid-cols-1 sm:grid-cols-2 gap-8">
+      <div class="flex flex-col gap-y-4">
+        <NuxtImg
+          class="object-cover w-full transition-transform duration-300 h-56 md:h-72 rounded-3xl"
+          :src="secondImageSrc"
+        />
+        <p class="md:text-lg text-pretty">
           {{ firstParagraph }}
         </p>
-        <NuxtImg class="object-cover w-full transition-transform duration-300 h-72 rounded-3xl" :src="secondImageSrc" />
       </div>
-      <div>
-        <p class="">
+      <div class="flex flex-col gap-y-4">
+        <NuxtImg
+          class="object-cover w-full transition-transform duration-300 h-56 md:h-72 rounded-3xl"
+          :src="thirdImageSrc"
+        />
+        <p class="md:text-lg text-pretty">
           {{ secondParagraph }}
         </p>
-        <NuxtImg class="object-cover w-full transition-transform duration-300 h-72 rounded-3xl" :src="thirdImageSrc" />
       </div>
     </div>
   </div>
--- pages/PolitiqueDeConfidentialite.vue
+++ pages/PolitiqueDeConfidentialite.vue
@@ -1,21 +1,21 @@
 <template>
-  <div class="page">
-    <h1>Politique de Confidentialité de Éducation Écologique pour un Terrain Confus</h1>
-    <p>
-      Chez Éducation Écologique, nous attachons une grande importance à la protection de votre vie privée et à la
+  <FooterPages
+    title="Politique de Confidentialité de Éducation Écologique pour un Terrain Confus"
+    firstParagraph="Chez Éducation Écologique, nous attachons une grande importance à la protection de votre vie privée et à la
       sécurité de vos données personnelles. Notre politique de confidentialité vise à vous informer sur la manière dont
-      nous collectons, utilisons et protégeons vos informations.
-    </p>
-    <p>
-      Nous nous engageons à ne jamais partager vos données personnelles avec des tiers sans votre consentement
+      nous collectons, utilisons et protégeons vos informations."
+    secondParagraph=" Nous nous engageons à ne jamais partager vos données personnelles avec des tiers sans votre consentement
       explicite. De plus, nous utilisons des mesures de sécurité avancées pour protéger vos informations contre tout
       accès non autorisé ou utilisation abusive. Si vous avez des questions ou des préoccupations concernant notre
-      politique de confidentialité, n'hésitez pas à nous contacter.
-    </p>
-  </div>
+      politique de confidentialité, n'hésitez pas à nous contacter."
+    firstImageSrc="/confidentiality.jpg"
+    secondImageSrc="/personnaldata.jpg"
+    thirdImageSrc="/donotshare.jpg"
+  />
 </template>
 
 <script setup>
+import FooterPages from "/components/FooterPages.vue";
 useHead({
   title: "Politique de Confidentialité - Éducation Écologique pour un Terrain Confus",
   meta: [
